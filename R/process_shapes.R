0b044130b7dd492f732922f849e85cb221042d1a
Neil Charles <neil.d.charles@gmail.com>
2022-03-28 22:33:13 +0100

    changed to identical geo_id col names

--- R/process_shapes.R
+++ R/process_shapes.R
@@ -1,12 +1,21 @@
 build_shapes <- function() {
   shapes_lad <-
-    sf::st_read("inst/extdata/shapes/la/la.shp")
+    sf::st_read("inst/extdata/shapes/la/la.shp") %>%
+    dplyr::rename(geo_id = cmlad11cd,
+                  geo_name = cmlad11nm) %>%
+    dplyr::select(geo_id, geo_name, geometry)
 
   shapes_msoa <-
-    sf::st_read("inst/extdata/shapes/msoa/msoa.shp")
+    sf::st_read("inst/extdata/shapes/msoa/msoa.shp") %>%
+    dplyr::rename(geo_id = MSOA11CD,
+                  geo_name = MSOA11NM) %>%
+    dplyr::select(geo_id, geo_name, geometry)
 
   shapes_lsoa <-
-    sf::st_read("inst/extdata/shapes/lsoa/lsoa.shp")
+    sf::st_read("inst/extdata/shapes/lsoa/lsoa.shp") %>%
+    dplyr::rename(geo_id = LSOA11CD,
+                  geo_name = LSOA11NM) %>%
+    dplyr::select(geo_id, geo_name, geometry)
 
   usethis::use_data(shapes_lad,
                     shapes_msoa,

9fd1e429124c8ab7cac174d01d37d011e6823b8e
Neil Charles <neil.d.charles@gmail.com>
2022-03-30 19:16:30 +0100

    Better shapes with data

--- R/process_census.R
+++ R/process_census.R
@@ -1,13 +1,13 @@
-export_shapes_and_data <- function(){
+export_facts <- function(){
 
-#Age Structure
-raw <- readr::read_csv("inst/extdata/stats/key_statistics/KS102EWDATA.CSV")
-meta <- readr::read_csv("inst/extdata/stats/key_statistics/KS102EW_2011STATH_NAT_OA_REL_1.4.4/KS102EWDESC0.CSV")
-# area_lookup <- read_csv("inst/extdata/lookup/PCD_OA_LSOA_MSOA_LAD_MAY21_UK_LU/PCD_OA_LSOA_MSOA_LAD_MAY21_UK_LU.csv") %>% #not in package because huge
-#   group_by(lsoa11cd, msoa11cd, ladcd, lsoa11nm, msoa11nm, ladnm, ladnmw) %>%
-#   summarise()
-#
-# usethis::use_data(area_lookup)
+area_lookup <- read_csv("inst/extdata/lookup/PCD_OA_LSOA_MSOA_LAD_MAY21_UK_LU/PCD_OA_LSOA_MSOA_LAD_MAY21_UK_LU.csv") %>% #not in package because huge
+  group_by(lsoa11cd, msoa11cd, ladcd, lsoa11nm, msoa11nm, ladnm, ladnmw) %>%
+  summarise()
+
+usethis::use_data(area_lookup)
+
+raw_lookup <- raw %>%
+  left_join(area_lookup)
 
 raw_long <- raw %>%
   tidyr::pivot_longer(-GeographyCode)
@@ -24,9 +24,9 @@ named_data <- raw_long %>%
 
 library(sf) #or the next left join doesn't work
 
-shapes_facts_lad <- shapes_lad %>%
-  dplyr::left_join(named_data, by = c("cmlad11cd" = "geography_code"))
+shapes_lad_facts <- shapes_lad %>%
+  dplyr::left_join(named_data, by = c("geo_id" = "geography_code"))
 
-usethis::use_data(shapes_facts_lad, overwrite = TRUE)
+usethis::use_data(shapes_lad_facts, overwrite = TRUE)
 
 }
--- R/process_shapes.R
+++ R/process_shapes.R
@@ -1,21 +1,40 @@
 build_shapes <- function() {
-  shapes_lad <-
-    sf::st_read("inst/extdata/shapes/la/la.shp") %>%
-    dplyr::rename(geo_id = cmlad11cd,
-                  geo_name = cmlad11nm) %>%
-    dplyr::select(geo_id, geo_name, geometry)
 
-  shapes_msoa <-
-    sf::st_read("inst/extdata/shapes/msoa/msoa.shp") %>%
-    dplyr::rename(geo_id = MSOA11CD,
-                  geo_name = MSOA11NM) %>%
-    dplyr::select(geo_id, geo_name, geometry)
+  lookup_areas <- readr::read_csv("inst/extdata/lookup/area_names/PCD_OA_LSOA_MSOA_LAD_MAY21_UK_LU.csv") %>% #not in package because huge
+    dplyr::group_by(lsoa11cd, msoa11cd, ladcd, lsoa11nm, msoa11nm, ladnm, ladnmw) %>%
+    dplyr::summarise() %>%
+    dplyr::ungroup()
+
+  usethis::use_data(lookup_areas, overwrite = TRUE)
 
   shapes_lsoa <-
     sf::st_read("inst/extdata/shapes/lsoa/lsoa.shp") %>%
     dplyr::rename(geo_id = LSOA11CD,
                   geo_name = LSOA11NM) %>%
-    dplyr::select(geo_id, geo_name, geometry)
+    dplyr::select(geo_id, geo_name, geometry) %>%
+    dplyr::left_join(
+      readr::read_rds("inst/extdata/stats/population/stats_population_lsoa.rds"), by = "geo_id"
+    )
+
+
+  shapes_lad <-
+    sf::st_read("inst/extdata/shapes/la/la.shp") %>%
+    dplyr::rename(geo_id = cmlad11cd,
+                  geo_name = cmlad11nm) %>%
+    dplyr::select(geo_id, geo_name, geometry) %>%
+    dplyr::left_join(
+      readr::read_rds("inst/extdata/stats/population/stats_population_lad.rds"), by = "geo_id"
+    )
+
+
+  shapes_msoa <-
+    sf::st_read("inst/extdata/shapes/msoa/msoa.shp") %>%
+    dplyr::rename(geo_id = MSOA11CD,
+                  geo_name = MSOA11NM) %>%
+    dplyr::select(geo_id, geo_name, geometry) %>%
+    dplyr::left_join(
+      readr::read_rds("inst/extdata/stats/population/stats_population_msoa.rds"), by = "geo_id"
+    )
 
   usethis::use_data(shapes_lad,
                     shapes_msoa,
